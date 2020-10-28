2a9f7e777b7128e9243e250a9f6707d2ab11897b
Matias <matiasdg202@gmail.com>
2020-10-28 20:42:44 -0300

    JoinTest completed

--- src/threads/JoinTest.java
+++ src/threads/JoinTest.java
@@ -3,29 +3,32 @@ package threads;
 public class JoinTest {
     public static void main(String[] args) {
         MyThread thread_0 = new MyThread();
-        MyThread thread_1 = new MyThread();
-        thread_0.start();
-        try {
-            thread_0.join();
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        MyThread thread_1 = new MyThread(thread_0);
         thread_1.start();
-        try {
-            thread_1.join();
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        thread_0.start();
         System.out.println("Programa terminado");
-
     }
 }
 
 class MyThread extends Thread {
 
-    public void start() {
+    public MyThread() {
+        t = null;
+    }
+
+    public MyThread(Thread thread) {
+        t = thread;
+    }
+
+    public void run() {
+        if (t != null) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
         for (int i = 0; i < 15; i++) {
             System.out.println("Thread en ejecución: " + getName());
             try {
@@ -36,4 +39,5 @@ class MyThread extends Thread {
             }
         }
     }
+    private Thread t;
 }
