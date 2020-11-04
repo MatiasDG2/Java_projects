5d1549e7f462a177f84440dc4f11a463fb3f73cb
Matias Gonzalez <matiasdg202@gmail.com>
2020-11-04 21:17:45 +0000

    Update LinkedListTest

--- src/collection/LinkedListTest.java
+++ src/collection/LinkedListTest.java
@@ -1,11 +1,21 @@
 package collection;
 
 import java.util.LinkedList;
-import java.util.Collection;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Iterator;
 
 public class LinkedListTest{
     public static void main(String[] args) {
-        c= new LinkedList<String>;
+        LinkedList <String> list= new LinkedList<String>();
+        list.add("Samuel");
+        list.add("Martin");
+        list.add("Pepe");
+        list.add("Roberto");
+        ListIterator<String> iterator= list.listIterator();
+        while (iterator.hasNext()){
+            String s= iterator.next();
+            System.out.println(s);
+        }
     }
-    private Collection c;
 }
