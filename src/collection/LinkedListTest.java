0e4b0a30f673110644b62e4747b03ed0160ac8c3
MatiasDG2 <70420306+MatiasDG2@users.noreply.github.com>
2020-11-04 16:53:15 -0300

    Update LinkedListTest.java

--- src/collection/LinkedListTest.java
+++ src/collection/LinkedListTest.java
@@ -1,1 +1,11 @@
+package collection;
 
+import java.util.LinkedList;
+import java.util.Collection;
+
+public class LinkedListTest{
+    public static void main(String[] args) {
+        c= new LinkedList<String>;
+    }
+    private Collection c;
+}
